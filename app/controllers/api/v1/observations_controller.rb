36682729f91b250f6e37f18eed2775b823917e05
Hari Kalanj <hkalanj@gmail.com>
2019-01-15 20:57:32 +0100

    configure serializers

--- app/controllers/api/v1/observations_controller.rb
+++ app/controllers/api/v1/observations_controller.rb
@@ -2,9 +2,12 @@ module Api::V1
   class ObservationsController < ApplicationController
 
     def index
-      @observations = Observation.all
-      render json: @observations
+      observations = []
+      Observation.all.each do |obs|
+        observations << ObservationSerializer.new(obs).serialized_json
+      end
+      render json: observations
     end
-    
+
   end
 end
