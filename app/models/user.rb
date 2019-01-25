06929aa59bb77ac98ab1d56f8f0b5b78f9f56b27
Hari Kalanj <hkalanj@gmail.com>
2019-01-25 17:06:21 +0100

    create user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@ class User < ApplicationRecord
 
   has_many :legated_observations, class_name: "Observation", foreign_key: "legator_id"
   has_many :determined_observations, class_name: "Observation", foreign_key: "determinator_id"
+  belongs_to :role, optional: true
 
   validates :username, :email, presence: true
   validates :email, uniqueness: true
+
+  def is_admin?
+    role.present? && role.name.downcase == "admin"
+  end
+
+  def is_moderator?
+    role.present? && role.name.downcase == "moderator"
+  end
 end
